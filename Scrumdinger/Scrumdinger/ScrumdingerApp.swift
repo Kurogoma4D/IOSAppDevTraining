1f5c3eaf38fd035b814afa766738f35a04c2df77
Kurogoma4D <highdro0479@gmail.com>
2022-02-05 01:34:10 +0900

    first commit

---
+++ Scrumdinger/Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Takafumi Suzuki on 2022/02/05.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MeetingView()
+        }
+    }
+}
